c895a7356f55ad464fbc884c5d168875ba063524
korka-dev <diallo30amadoukorka@gmail.com>
2025-03-14 14:40:04 +0000

    faire un roolback pour tester le deployement

---
+++ frontend/src/app/components/SuspenseWrapper.tsx
@@ -0,0 +1,9 @@
+// SuspenseWrapper.tsx
+import React, { Suspense } from 'react';
+
+const SuspenseWrapper = ({ children }: { children: React.ReactNode }) => {
+  return <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>;
+};
+
+export default SuspenseWrapper;
+
